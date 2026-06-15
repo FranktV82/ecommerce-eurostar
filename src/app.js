2822f2036b02e6144a8e65a356d5898298536283
FranktV82 <40358301+FranktV82@users.noreply.github.com>
2026-06-15 11:44:38 +0200

    before testautomation

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const checkoutRoutes = require('./routes/checkoutRoutes');
 const healthRoutes = require('./routes/healthRoutes');
+const swaggerRoutes = require('./routes/swaggerRoutes');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/api-docs', swaggerRoutes);
 app.use(authRoutes);
 app.use(checkoutRoutes);
 app.use(healthRoutes);
